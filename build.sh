41c9b4c686135f6019c2c576fdb310cc7b1d144f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-01-05 00:10:30 +0200

    Refactor build script to separate static and dynamic archive packaging for Windows and Unix

--- build.sh
+++ build.sh
@@ -2,22 +2,34 @@
 
 cd espeak-ng
 
-# Static archive
-cmake -B build -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTS=OFF -DCOMPILE_INTONATIONS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=_static .
-cmake --build build
-cmake --install build
-
 # Dynamic archive
 cmake -B build -DBUILD_SHARED_LIBS=ON -DENABLE_TESTS=OFF -DCOMPILE_INTONATIONS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=_dynamic .
-cmake --build build
+cmake --build build --config Release
 cmake --install build
+
+# Static archive (not for Windows)
+if [[ ! "$(uname -s)" =~ MINGW|MSYS|CYGWIN ]]; then
+    cmake -B build -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTS=OFF -DCOMPILE_INTONATIONS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=_static .
+    cmake --build build --config Release
+    cmake --install build
+fi
 
 # Remove unnecessary files
 rm -rf _dynamic/share
-# Copy the static library to the dynamic library
-cp _static/lib/libespeak-ng.a _dynamic/lib
-# Package the libraries
-mv _dynamic espeak-ng-libs
-tar -czf espeak-ng-libs.tar.gz espeak-ng-libs
 
+
+# Package for Windows
+if [[ "$(uname -s)" =~ MINGW|MSYS|CYGWIN ]]; then
+    cp _dynamic/bin/espeak-ng.dll _dynamic/lib
+    rm -rf _dynamic/lib/pkgconfig
+    mv _dynamic espeak-ng-libs
+else
+    # Package for Unix
+    cp _static/lib/libespeak-ng.a _dynamic/lib
+    # Package the libraries
+    mv _dynamic espeak-ng-libs
+fi
+
+# Create the tarball
+tar -czf espeak-ng-libs.tar.gz espeak-ng-libs
 echo "espeak-ng-libs.tar.gz created"
