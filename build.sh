6ff0dbddc2674daa368242b93e13c63674a301c6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-01-06 03:55:28 +0200

    Comment out static build steps in build.sh for clarity and future reference

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ cmake --build build --config Release
 cmake --install build
 
 # Static archive (not for Windows)
-if [[ ! "$(uname -s)" =~ MINGW|MSYS|CYGWIN ]]; then
-    cmake -B build -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTS=OFF -DCOMPILE_INTONATIONS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=_static . $EXTRA_CMAKE_ARGS
-    cmake --build build --config Release
-    cmake --install build
-fi
+# if [[ ! "$(uname -s)" =~ MINGW|MSYS|CYGWIN ]]; then
+#     cmake -B build -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTS=OFF -DCOMPILE_INTONATIONS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=_static . $EXTRA_CMAKE_ARGS
+#     cmake --build build --config Release
+#     cmake --install build
+# fi
 
 # Remove unnecessary files
 rm -rf _dynamic/share
@@ -25,7 +25,7 @@ if [[ "$(uname -s)" =~ MINGW|MSYS|CYGWIN ]]; then
     mv _dynamic espeak-ng-libs
 else
     # Package for Unix
-    cp _static/lib/libespeak-ng.a _dynamic/lib
+    # cp _static/lib/libespeak-ng.a _dynamic/lib
     # Package the libraries
     mv _dynamic espeak-ng-libs
 fi
